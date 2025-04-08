1df9125c4916533a19233760c2e97394136759b7
v.skurikhin <v.skurikhin@innopolis.ru>
2025-04-08 18:57:08 +0700

    first commit

---
+++ app/app.py
@@ -0,0 +1,20 @@
+# from langchain.chains import LLMChain
+# from langchain.llms import OpenAI
+# from langchain.prompts import PromptTemplate
+
+# Инициализация модели OpenAI
+
+# # Создание шаблона для промта
+# prompt_template = PromptTemplate(
+#     input_variables=["topic"],
+#     template="Напиши короткий рассказ на тему: {topic}"
+# )
+
+prompt_template = 'a'
+# Вывод результата
+print(prompt_template)
+
+while True:
+    from time import sleep
+    print(prompt_template)
+    sleep(1)
